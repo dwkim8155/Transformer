4281e2e91e9c975ed2d871c8d9202776cc02107a
dwkim8155 <dwkim8155@naver.com>
2024-07-08 21:02:21 +0900

    chore: Refactor generate_sentc function and add softmax for better prediction

--- utils.py
+++ utils.py
@@ -23,36 +23,43 @@ def shift_right(tensor, bos_token_id):
     return torch.cat([bos_token, tensor[:, :-1]], dim=-1)
 
 
+import torch
+
 def generate_sentc(model, sp, n_seq, prompt, device):
     enc_ids = sp.encode_as_ids(prompt)
     if len(enc_ids) > n_seq:
         print("입력하신 문장이 너무 깁니다. 다시 입력해주세요.")
         return
-    enc_ids += [0] * (n_seq - len(enc_ids))
-    # 초기 디코더 입력 설정 (BOS 토큰으로 시작)
-    dec_ids = [sp.bos_id()] + [0] * (n_seq - 1)
-    
-    # Tensor로 변환하고 디바이스에 할당
+
+    enc_ids += [0] * (n_seq - len(enc_ids))  # 패딩 추가
+    dec_ids = [sp.bos_id()] + [0] * (n_seq - 1)  # 초기 디코더 입력 설정
+
     enc_ids = torch.tensor(enc_ids).unsqueeze(0).to(device)
     dec_ids = torch.tensor(dec_ids).unsqueeze(0).to(device)
+    soft_max = torch.nn.Softmax(dim=-1).to(device)  # dim=-1로 수정
     
-    # Transformer 디코더 루프
     for i in range(n_seq - 1):
         output = model(enc_ids, dec_ids)
         
         # 다음 토큰 예측
-        pred = output[0, i].argmax().item()
+        output = soft_max(output[:, -1, :])  # 마지막 시퀀스 위치에 대해서 softmax 적용
+        pred = output.argmax(dim=-1).item()
         
-        # EOS 토큰을 예측한 경우 중단
         if pred == sp.eos_id():
-            break
+            return sp.decode_ids(dec_ids[0].tolist())
         
         # 다음 디코더 입력 업데이트
         dec_ids[0, i + 1] = pred
-    # 최종 출력 문장 디코딩
-    generated_ids = dec_ids[0].tolist()
-    generated_sentence = sp.DecodeIds(generated_ids)
 
+    final_output = model(enc_ids, dec_ids)
+    final_output = soft_max(final_output[:, -1, :])
+    pred = final_output.argmax(dim=-1).item()
+    
+    #dec_ids에서 BOS 제거하고 마지막 pred 추가
+    
+    generated_id = dec_ids[0].tolist()
+    generated_id.pop(0)
+    generated_id.append(pred)
+    generated_sentence = sp.decode_ids(generated_id)
+    
     return generated_sentence
-
-        
